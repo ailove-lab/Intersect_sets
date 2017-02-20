5411452c4de263bb231b9bc0e3acaf1519f8aecf
peko <gasu.basu@gmail.com>
2017-02-21 02:12:18 +0300

    update

--- uids_activity.cpp
+++ uids_activity.cpp
@@ -10,13 +10,13 @@
 // iterate over interview folder, collect ids to map type -> ids
 // inerview_name -> uids vector -> activity_set
 
-#include <iostream>
-#include <fstream>
-#include <set>
-#include <map>
-#include <vector>
-#include <array>
 #include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
 
 #include <dirent.h>
 
@@ -24,108 +24,113 @@
 
 using namespace std;
 
-int usage();
+int  usage();
 bool get_allfiles(string dir_name, vector<string>& files);
-//  interview   uid     activity
-set<string>                       interviews_set;
-map<string, set<string>>          interviews_map;
-map<string, array<int, MAX_BITS>> interviews_stat;
+
+set<string> interviews_set;              // Уникальные uid'ы
+map<string, set<string>> interviews_map; // файл -> uid'ы
+map<string, array<int, MAX_BITS>> interviews_stat; // файл -> статистика
 
 int main(int argc, char** argv) {
 
-  if(argc < 3) return usage();
+    if (argc < 3)
+        return usage();
 
+    // Путь к директории с файлами опроса
+    string path = argv[1]; // interview ids
+    // Файлы опроса
+    vector<string> files;
+    get_allfiles(path, files);
+    sort(files.begin(), files.end());
+    for (auto&& f : files)
+        cerr << f << endl;
 
-  // get interview
+    for (auto&& f : files) {
+        string   filename = path + "/" + f;
+        ifstream file(filename);
+        if (file.is_open()) {
+            // инициализация статистики
+            interviews_map[f] = set<string>{}; // Уникальные uid'ы файла
+            interviews_stat[f] = array<int, MAX_BITS>{0}; // статистика файла
 
-  string path = argv[1]; // interview ids
-  vector<string> files;
-  get_allfiles(path, files);
-  sort(files.begin(), files.end());
-  for(auto&& f : files) cerr << f << endl;
-  for(auto&& f : files) {
-    string filename = path + "/" + f;
-    ifstream file(filename);
-    if (file.is_open()) {
-      interviews_map[f]  = set<string>{};           // init map
-      interviews_stat[f] = array<int, MAX_BITS>{0}; // init stat
-      string uid;
-      // iterate throught lines
-      while (getline(file, uid)) {
-        interviews_set.insert(uid);    // total uid list
-        interviews_map[f].insert(uid); // per list uid list
-        if(uid[uid.size()-1]=='\r') uid.resize(uid.size()-1);
-      }
-    } else {
-      cerr << "can't open " << filename << endl;
-    }
-  }
-
-
-  // get activity
-
-  files.clear();
-  path = argv[2]; // activity ids
-  get_allfiles(path, files);
-  sort(files.begin(), files.end());
-  for(auto&& f : files) cerr << f << endl;
-  int bit = 0;
-  // iterate throught files
-  for(auto&& f : files) {
-    string filename = path + "/" + f;
-    ifstream file(filename);
-    if (file.is_open()) {
-      string uid;
-      // iterate throught lines
-      while (getline(file, uid)) {
-        if(uid[uid.size()-1]=='\r') uid.resize(uid.size()-1);
-        // check if uid beloving interviewers
-        if(interviews_set.find(uid) != interviews_set.end()) {
-          // find interviewer at list
-          for(auto&& i : interviews_map) {
-            // if interview has uid, increment col
-            if(i.second.find(uid) != i.second.end()) {
-              interviews_stat[i.first][bit]++;
+            string uid;
+            // iterate throught lines
+            while (getline(file, uid)) {
+                interviews_set.insert(uid);      // Собираем уникальные uid'ы
+                interviews_map[f].insert(uid);   // Собираем уникальные uid'ы в файле
+                if (uid[uid.size() - 1] == '\r') // cleanup \r at the end
+                    uid.resize(uid.size() - 1);
             }
-          }
+        } else {
+            cerr << "can't open " << filename << endl;
         }
-      }
-      bit++;
-    } else {
-      cerr << "cant't open " << filename << endl;
     }
-  }
 
+    // Папка с активностью пользователей
+    files.clear();
+    path = argv[2]; // activity ids
+    get_allfiles(path, files);
+    sort(files.begin(), files.end());
+    for (auto&& f : files)
+        cerr << f << endl;
 
- // пробегаем по уидам, выводим
-  for(auto&& s : interviews_stat) {
-    cout << s.first << " ";
-    for(auto&& a : s.second) {
-      cout << a << " ";
+    int bit = 0;
+    // Проходим по всем файлам/датам
+    for (auto&& f : files) {
+        string   filename = path + "/" + f;
+        ifstream file(filename);
+        if (file.is_open()) {
+            string uid;
+            // Для каждого uid'a
+            while (getline(file, uid)) {
+                if (uid[uid.size() - 1] == '\r') // очищаем \r в конце
+                    uid.resize(uid.size() - 1);
+                // Проверяем был ли такой пользователь в опросе
+                if (interviews_set.find(uid) != interviews_set.end()) {
+                    // Если был, находим категорию ответа
+                    for (auto&& i : interviews_map) {
+                        // И инкрементируем её счетчик
+                        if (i.second.find(uid) != i.second.end()) {
+                            interviews_stat[i.first][bit]++;
+                        }
+                    }
+                }
+            }
+            // следующая категория
+            bit++;
+        } else {
+            cerr << "cant't open " << filename << endl;
+        }
     }
-    cout << endl;
-  }
 
-  return 0;
+    // пробегаем по уидам, выводим
+    for (auto&& s : interviews_stat) {
+        cout << s.first << " ";
+        for (auto&& a : s.second) {
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
-
 
 // Получаем список файлов
 bool get_allfiles(string dir_name, vector<string>& files) {
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir (dir_name.c_str())) != NULL) {
-        while ((ent = readdir (dir)) != NULL) {
-            if(ent->d_name[0] == '.') continue;
+    DIR*           dir;
+    struct dirent* ent;
+    if ((dir = opendir(dir_name.c_str())) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_name[0] == '.')
+                continue;
             files.push_back(ent->d_name);
         }
-        closedir (dir);
+        closedir(dir);
         return 1;
     }
 
-    cerr << "Не могу открыть каталог "<< dir_name;
+    cerr << "Не могу открыть каталог " << dir_name;
     return 0;
-
 }
 
 // инструкция
